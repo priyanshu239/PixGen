4f53f7cae523796f6c88f280a63728c27d56eb63
priyanshu239 <priyanshuguptaablack@gmail.com>
2025-11-24 19:11:54 +0530

    Implement Razorpay payment integration and update credit plans

--- server/models/transactionModel.js
+++ server/models/transactionModel.js
@@ -4,6 +4,7 @@ const transactionSchema = new mongoose.Schema({
     userId: {type: String, required:true},
     plan: {type: String, required:true},
     amount: {type: Number, required:true},
+    credits: {type: Number, default: 0},
     payments: {type: Boolean, default:false},
     date: {type: Number},
 })
